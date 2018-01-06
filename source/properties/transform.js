3d048e8fb9dc98f0fedb9d6767d051c5d89b5908
Hemerson Vianna <hemerson.lourenco@gmail.com>
2018-01-06 15:55:23 -0200

    feat: add transform property

---
+++ source/properties/transform.js
@@ -0,0 +1,87 @@
+const transform = keys => {
+  let fullValue = keys[0].value;
+  // remove espaços que estejam dentro de valores de transformação
+  fullValue = fullValue.replace(/\((.+?)\)/g, function(a, b) {
+      return "(" + b.replace(/\s/g, "") + ")";
+  });
+  // os únicos espaços deixados devem estar entre valores de transformação
+  let values = fullValue.split(' ');
+  let transformations = [];
+
+  for (let i = 0, j = values.length; i < j; i++) {
+    let matches = values[i].match(/(.+)\((.+)\)/);
+    let transformationType = matches[1];
+    let transformation = matches[2];
+
+    switch (transformationType) {
+      case "perspective":
+        transformations.push({
+          perspective: parseFloat(transformation)
+        })
+        break;
+
+      case "rotate":
+      case "rotateX":
+      case "rotateY":
+      case "rotateZ":
+        let thisTransformation = {};
+        thisTransformation[transformationType] = transformation;
+        transformations.push(thisTransformation);
+        break;
+
+      case "rotate3d":
+      case "rotate3D":
+        let thisTransformationValues = transformation.split(",");
+        transformations.push(
+          { rotateX: thisTransformationValues[0] },
+          { rotateY: thisTransformationValues[1] },
+          { rotateZ: thisTransformationValues[2] }
+        );
+        break;
+
+      case "scale":
+      case "scaleX":
+      case "scaleY":
+        let thisTransformation = {};
+        thisTransformation[transformationType] = parseFloat(transformation);
+        transformations.push(thisTransformation);
+        break;
+
+      case "scale3d":
+      case "scale3D":
+      case "scale2d":
+      case "scale2D":
+        let thisTransformationValues = transformation.split(",");
+        transformations.push(
+          { scaleX: parseFloat(thisTransformationValues[0]) },
+          { scaleY: parseFloat(thisTransformationValues[1]) }
+        );
+        break;
+
+      case "translateX":
+      case "translateY":
+        let thisTransformation = {};
+        thisTransformation[transformationType] = parseFloat(transformation);
+        transformations.push(thisTransformation);
+        break;
+
+      case "translate3d":
+      case "translate3D":
+      case "translate2d":
+      case "translate2D":
+        let thisTransformationValues = transformation.split(",");
+        transformations.push(
+          { translateX: parseFloat(thisTransformationValues[0]) },
+          { translateY: parseFloat(thisTransformationValues[1]) }
+        );
+        break;
+      }
+  }
+
+  return [{
+    key: "transform",
+    value: transformations
+  }];
+};
+
+module.exports =  transform;
